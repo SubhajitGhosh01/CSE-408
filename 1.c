69cd5954dab9be52362bd9d657f107e7a5e64c7d
SubhajitGhosh01 <subhajitghosh.official01@gmail.com>
2026-04-29 08:59:25 +0530

    second commit

---
+++ 1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s", s);
+    int n = strlen(s), start = 0, maxlen = 1;
+
+    for(int i = 0; i < n; i++) {
+        int l = i, r = i;
+        while(l >= 0 && r < n && s[l] == s[r]) {
+            if(r - l + 1 > maxlen) {
+                start = l;
+                maxlen = r - l + 1;
+            }
+            l--; r++;
+        }
+        l = i; r = i + 1;
+        while(l >= 0 && r < n && s[l] == s[r]) {
+            if(r - l + 1 > maxlen) {
+                start = l;
+                maxlen = r - l + 1;
+            }
+            l--; r++;
+        }
+    }
+
+    for(int i = start; i < start + maxlen; i++) printf("%c", s[i]);
+    return 0;
+}
